49fd346c1b71c3968260f24b1ce7723eab36c4eb
Mkdenno <denniskiprop25@gmail.com>
2024-03-21 08:06:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/signindemo/components/primaryButton.kt
@@ -0,0 +1,28 @@
+package com.example.signindemo.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PrimaryButton(
+
+    modifier: Modifier =Modifier,
+    label:String,
+    onClick:()-> Unit
+) {
+    Button(onClick = onClick,
+    modifier=Modifier
+        .fillMaxWidth(),
+        shape= RoundedCornerShape(8.dp)
+
+    ) {
+      Text(text = label)
+    }
+
+}
---
+++ app/src/main/java/com/example/signindemo/signin/signIn.kt
@@ -0,0 +1,59 @@
+package com.example.signindemo.signin
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.signindemo.components.Input
+import com.example.signindemo.components.PassWordInput
+import com.example.signindemo.components.PrimaryButton
+
+@Composable
+fun SignInScreen() {
+
+    var username by remember {mutableStateOf("")}
+    var password by remember {mutableStateOf("")}
+    Column(modifier = Modifier
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+     Text(text = "SIGN IN", fontSize =28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier .height(8.dp))
+
+        Input(label = "Username", placeholder ="Username" , value =username, onValueChange ={
+            username=it
+        } )
+
+        Spacer(modifier = Modifier
+            .height(8.dp)
+        )
+
+        PassWordInput(value = password, onValueChange = {
+            password=it
+        })
+
+        Spacer(modifier = Modifier
+            .height(8.dp)
+        )
+
+        PrimaryButton(label = "SIGN IN") {
+
+        }
+    }
+
+}
